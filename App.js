8bc104c5ccc4e2dd3886c358f37dcf20cc42bc49
gawd-coder <ansari.ammar786@gmail.com>
2020-05-30 05:48:47 +0530

    App ready with hardcoded data

--- App.js
+++ App.js
@@ -1,36 +1,91 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  Text,
+  Platform,
+} from 'react-native';
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+import getImageForWeather from './utils/getImageForWeather';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
-  );
+import SearchInput from './components/SearchInput';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: '',
+    };
+  }
+
+  componentDidMount(){
+    this.handleUpdateLocation("San Francisco")
+  }
+
+  handleUpdateLocation = city => {
+    this.setState({
+      location: city,
+    });
+  };
+
+  render() {
+    const { location } = this.state;
+
+    return (
+      <View style={styles.container} behavior="padding">
+        <ImageBackground
+          source={getImageForWeather('Clear')}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+          <View style={styles.detailsContainer}>
+            <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
+            <Text style={[styles.smallText, styles.textStyle]}>
+              Light Cloud
+            </Text>
+            <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+
+            <SearchInput
+              placeholder="Search any city"
+              onSubmit={this.handleUpdateLocation}
+            />
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const styles  = StyleSheet.create({
+   container: {
+     flex: 1,
+     backgroundColor: '#FEEAE6',
+   },
+     textStyle: {
+     textAlign: 'center',
+     fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+     color: "#442C2E"
+   },
+     largeText: {
+     fontSize: 44,
+   },
+     smallText: {
+     fontSize: 18,
+   },
+//  flex is responsible for image to cover full width and resize mode makes it look like that covering full screen on zooming
+   imageContainer: {
+     flex: 1,
+   },
+   image: {
+     flex: 1,
+     width: null,
+     height: null,
+     resizeMode: 'cover',
+   },
+   detailsContainer: {
+     flex: 1,
+     justifyContent: 'center',
+     backgroundColor: 'rgba(0,0,0,0.2)',
+     paddingHorizontal: 20,
+     },
+})
---
+++ components/SearchInput.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+import PropTypes from 'prop-types';
+
+export default class SearchInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    };
+  }
+
+  handleChangeText = text => {
+    this.setState({ text });
+  };
+
+  handleSubmitEditing = () => {
+    const { onSubmit } = this.props;
+    const { text } = this.state;
+
+    if (!text) return;
+
+    onSubmit(text);
+    this.setState({ text: '' });
+  };
+
+  render() {
+    const { placeholder } = this.props;
+    const { text } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <TextInput
+          autoCorrect={false}
+          value={text}
+          placeholder={placeholder}
+          placeholderTextColor="#442C2E"    
+          style={styles.textInput}
+          clearButtonMode="always"
+          onChangeText={this.handleChangeText}
+          onSubmitEditing={this.handleSubmitEditing}
+        />
+      </View>
+    );
+  }
+}
+
+SearchInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+};
+
+SearchInput.defaultProps = {
+  placeholder: '',
+};
+
+const styles  = StyleSheet.create({
+    textInput: {
+        backgroundColor: '#FEDBD0',
+        color :"#442C2E",
+        height: 40,
+        width: 300,
+        marginTop: 20,
+        marginHorizontal: 20,
+        paddingHorizontal: 10,
+        alignSelf: 'center',
+        borderRadius: 10,
+    },
+})
